5c5bc0742e4df692a408da8b12ad385fc046fa1b
pla <massenpost@pla1.de>
2025-01-22 19:08:20 +0100

    unified sile inventory to 256 stacks

--- data.lua
+++ data.lua
@@ -3,3 +3,10 @@ if settings.startup["angels-enable-warehouses"].value then
   data.raw["logistic-container"]["angels-warehouse-buffer"].inventory_size = 786
   data.raw["logistic-container"]["angels-warehouse-requester"].inventory_size = 786
 end
+
+if settings.startup["angels-enable-silos"].value then
+  data.raw["logistic-container"]["silo-active-provider"].inventory_size = 256
+  data.raw["logistic-container"]["silo-storage"].inventory_size = 256
+  data.raw["logistic-container"]["silo-requester"].inventory_size = 256
+
+end
